5f2fe49c3459d03fb5453f9b08cb772ecabe5cf7
Brad Eckert <beckert@mit.edu>
2014-01-09 21:07:43 -0800

    Working locally

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,10 @@
 //= require_tree .
+
+App = Ember.Application.create();
+
+App.Router.map(function() {
+	this.resource('about');
+	this.resource('projects');
+	this.resource('resume');
+	this.resource('contact');
+});
